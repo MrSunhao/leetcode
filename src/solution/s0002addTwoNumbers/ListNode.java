1d809346da846ff25571971220e1183465d5da6e
MrSunhao <sun668vip@163.com>
2019-07-12 22:53:05 +0800

    add a new question,name is add two numbers .

---
+++ src/solution/s0002addTwoNumbers/ListNode.java
@@ -0,0 +1,10 @@
+package solution.s0002addTwoNumbers;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
--- src/solution/s0002addTwoNumbers/Main.java
+++ src/solution/s0002addTwoNumbers/Main.java
@@ -2,6 +2,27 @@ package solution.s0002addTwoNumbers;
 
 public class Main {
     public static void main(String[] args) {
+        ListNode node1 = new ListNode(2);
+        ListNode node = node1;
+        node.next = new ListNode(4);
+        node = node.next;
 
+        node.next = new ListNode(3);
+        node = node.next;
+
+        ListNode node2 = new ListNode(5);
+        node = node2;
+        node.next = new ListNode(6);
+        node = node.next;
+
+        node.next = new ListNode(4);
+        node = node.next;
+
+        Solution solution = new Solution();
+        ListNode result = solution.addTwoNumbers(node1, node2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
     }
 }
