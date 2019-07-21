38a66bec457f9cb5a6eb07a4a70d0084d93cc9cf
MrSunhao <sun668vip@163.com>
2019-07-21 22:12:03 +0800

    add longest substring with repeating characters .

--- src/solution/s0003longestSubstringWithoutRepeatingCharacters/Main.java
+++ src/solution/s0003longestSubstringWithoutRepeatingCharacters/Main.java
@@ -2,6 +2,8 @@ package solution.s0003longestSubstringWithoutRepeatingCharacters;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        String str = "abcabcbb";
+        System.out.println(solution.lengthOfLongestSubstring1(str));
     }
 }
--- src/solution/s0003longestSubstringWithoutRepeatingCharacters/Solution.java
+++ src/solution/s0003longestSubstringWithoutRepeatingCharacters/Solution.java
@@ -1,4 +1,64 @@
 package solution.s0003longestSubstringWithoutRepeatingCharacters;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
+    /***
+     * @author sunth
+     * @date 2019/7/15 0:05
+     * @Description
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int res = 0;
+        int end=0,start=0;
+        Set<Character> set=new HashSet<>();
+        while(start<n && end<n){
+            if(set.contains(s.charAt(end))){
+                set.remove(s.charAt(start++));
+            }else{
+                set.add(s.charAt(end++));
+                res=Math.max(res,end-start);
+            }
+
+        }
+        return res;
+    }
+
+
+    /***
+     * @author sunth
+     * @date 2019/7/15 0:05
+     * @Description 解法1
+     */
+    public int lengthOfLongestSubstring1(String s) {
+        if (!s.isEmpty()) {
+            char[] chars = s.toCharArray();
+            if (chars.length == 1) {
+                return 1;
+            }
+            int length = 0;
+            String str = "";
+            int j = 0;
+            for (int i = 0; i < chars.length; i++) {
+                if (!str.contains(String.valueOf(chars[i]))) {
+                    str += String.valueOf(chars[i]);
+                    if (str.length() > length) {
+                        length = str.length();
+                    }
+                } else {
+                    if (str.length() > length) {
+                        length = str.length();
+                    }
+                    j++;
+                    i = j;
+                    str = String.valueOf(chars[i]);
+                }
+            }
+            return length;
+        }
+        return 0;
+    }
 }
